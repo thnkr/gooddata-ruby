88debdc88c58330a245b32374e2ced83b6b74c56
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2014-03-13 22:20:06 +0100

    Added a simple brick test

--- spec/bricks/bricks_spec.rb
+++ spec/bricks/bricks_spec.rb
@@ -5,4 +5,23 @@ describe GoodData::Bricks do
   it "Has GoodData::Bricks::Brick class" do
     GoodData::Bricks::Brick.should_not == nil
   end
+
+  it "should be possible to execute a brick" do
+    
+    class DummyBrick < GoodData::Bricks::Brick
+
+      def call(params)
+        puts "hello"
+      end
+    end
+
+    include GoodData::Bricks
+
+    p = GoodData::Bricks::Pipeline.prepare([
+      BenchMiddleware,
+      DummyBrick])
+
+    p.call({})
+  end
+
 end
